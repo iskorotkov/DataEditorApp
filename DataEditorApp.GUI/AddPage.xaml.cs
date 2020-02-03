709cdf4e660c7308916d31382fcfb6f6c7989368
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-03 23:31:50 +0500

    Make add form add user to the DB

--- DataEditorApp.GUI/AddPage.xaml.cs
+++ DataEditorApp.GUI/AddPage.xaml.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace DataEditorApp.GUI
 {
@@ -18,9 +7,12 @@ namespace DataEditorApp.GUI
     /// </summary>
     public partial class AddPage : Page
     {
+        private readonly AddPagePresenter _presenter;
+
         public AddPage()
         {
             InitializeComponent();
+            _presenter = new AddPagePresenter(this);
         }
     }
 }
