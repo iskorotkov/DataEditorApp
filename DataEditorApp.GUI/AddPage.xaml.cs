c70063c0d659903e299a773f19b374f0b33c1d39
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-03 20:05:04 +0500

    Add empty form for adding new users

---
+++ DataEditorApp.GUI/AddPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataEditorApp.GUI
+{
+    /// <summary>
+    /// Interaction logic for AddPage.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        public AddPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
