f87dd209bcb587b63339e13548a296d674b0d092
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-04 10:58:47 +0500

    Add a page for viewing list of users

--- DataEditorApp.GUI/ViewPagePresenter.cs
+++ DataEditorApp.GUI/ViewPagePresenter.cs
@@ -1,12 +1,45 @@
-﻿namespace DataEditorApp.GUI
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using DataEditorApp.Users;
+using DataEditorApp.View.Postgres;
+using Npgsql;
+
+namespace DataEditorApp.GUI
 {
     public class ViewPagePresenter
     {
         private readonly ViewPage _page;
-        
+
         public ViewPagePresenter(ViewPage page)
         {
             _page = page;
+
+            // TODO: Populate users list only when this page gets active
+            PopulateUsersList();
+        }
+
+        private void PopulateUsersList()
+        {
+            var conStr = new UsersConnectionStringBuilder().Build();
+            try
+            {
+                List<User> users;
+                using (var con = new NpgsqlConnection(conStr))
+                {
+                    con.Open();
+                    users = new GetAllUsersCommand(con).Execute();
+                }
+
+                _page.UsersLv.Items.Clear();
+                users.ForEach(u =>
+                    _page.UsersLv.Items.Add(
+                        $"User #{u.Id}: login={u.Login}, creation date={u.CreationDate.Day}.{u.CreationDate.Month}.{u.CreationDate.Year}"));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error while reading users data");
+            }
         }
     }
 }
---
+++ DataEditorApp.View.Postgres/GetAllUsersCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using DataEditorApp.Users;
+using Npgsql;
+
+namespace DataEditorApp.View.Postgres
+{
+    public class GetAllUsersCommand
+    {
+        private readonly NpgsqlCommand _command;
+        
+        public GetAllUsersCommand(NpgsqlConnection con)
+        {
+            _command = new NpgsqlCommand
+            {
+                Connection = con,
+                CommandText = @"SELECT users.id,
+                                       users.login,
+                                       users.creation_date
+                                FROM users"
+            };
+        }
+
+        public List<User> Execute()
+        {
+            var users = new List<User>();
+            using (var reader = _command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var user = new User
+                    {
+                        Id = (int) reader["id"],
+                        Login = (string) reader["login"],
+                        CreationDate = (DateTime) reader["creation_date"]
+                    };
+                    users.Add(user);
+                }
+            }
+
+            return users;
+        }
+    }
+}
