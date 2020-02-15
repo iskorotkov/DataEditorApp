be48a92e644979c099f203369dde388dca4a7cfb
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-15 23:30:59 +0500

    Fix validation errors when 'allow empty' was true

--- DataEditorApp.Controls.Validation/ValidationBox.cs
+++ DataEditorApp.Controls.Validation/ValidationBox.cs
@@ -6,29 +6,37 @@ namespace DataEditorApp.Controls.Validation
 {
     public abstract class ValidationBox : UserControl
     {
+        private bool _allowEmpty;
         protected TextBoxDecorator? Decorator { get; private set; }
 
-        public bool IsCorrect => Decorator.IsCorrect;
+        public bool IsCorrect => Decorator.IsCorrect || AllowEmpty && Text.Length == 0;
         public abstract string Text { get; set; }
         public TextBoxDecorator.OnIsCorrectChangedDelegate OnIsCorrectChanged => Decorator.OnIsCorrectChanged;
         public Predicate<string>? IsAvailable { get; set; } = null;
         public void ResetInputBox() => Decorator.Reset();
-        public bool AllowEmpty { get; set; } = true;
+
+        public bool AllowEmpty
+        {
+            get => _allowEmpty;
+            set
+            {
+                _allowEmpty = value;
+                OnTextChanged();
+            }
+        }
 
         protected void RegisterTextBox(TextBox inputBox)
         {
             Decorator = new TextBoxDecorator(inputBox);
             inputBox.TextChanged += (sender, args) => OnTextChanged();
-            if (AllowEmpty)
-                Decorator.InputIsCorrect();
+            OnTextChanged();
         }
 
         protected void RegisterPasswordBox(PasswordBox inputBox)
         {
             Decorator = new TextBoxDecorator(inputBox);
             inputBox.PasswordChanged += (sender, args) => OnTextChanged();
-            if (AllowEmpty)
-                Decorator.InputIsCorrect();
+            OnTextChanged();
         }
 
         protected abstract void OnTextChanged();
