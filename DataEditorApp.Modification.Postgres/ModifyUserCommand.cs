0c94dd4c9235a7d48cd0d9a8da3663afa33b2537
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-15 20:34:08 +0500

    Add command to modify user data in DB

---
+++ DataEditorApp.Modification.Postgres/ModifyUserCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Npgsql;
+
+namespace DataEditorApp.Modification.Postgres
+{
+    public class ModifyUserCommand
+    {
+        private readonly NpgsqlCommand _command;
+
+        public ModifyUserCommand(NpgsqlConnection con, int id, string login, DateTime creationDate)
+        {
+            _command = new NpgsqlCommand
+            {
+                Connection = con,
+                CommandText = @"UPDATE users
+                                SET login         = @login,
+                                    creation_date = @creation_date
+                                WHERE id = @id;"
+            };
+            _command.Parameters.AddWithValue("@id", id);
+            _command.Parameters.AddWithValue("@login", login);
+            _command.Parameters.AddWithValue("@creation_date", creationDate);
+        }
+
+        public void Execute() => _command.ExecuteNonQuery();
+    }
+}
