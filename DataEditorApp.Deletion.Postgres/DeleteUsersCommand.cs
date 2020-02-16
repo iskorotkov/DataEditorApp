9668f948394509b63eb76deb8b186b4abba2e78e
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-16 09:33:44 +0500

    Add deletion command

---
+++ DataEditorApp.Deletion.Postgres/DeleteUsersCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataEditorApp.Users;
+using Npgsql;
+
+namespace DataEditorApp.Deletion.Postgres
+{
+    public class DeleteUsersCommand
+    {
+        private readonly NpgsqlCommand _command;
+        
+        public DeleteUsersCommand(NpgsqlConnection con, List<User> selectedUsers)
+        {
+            _command = new NpgsqlCommand
+            {
+                Connection = con,
+                CommandText = @"DELETE
+                                FROM users
+                                WHERE users.id = ANY(@id_list)"
+            };
+            var ids = selectedUsers.Select(u => u.Id).ToArray();
+            _command.Parameters.AddWithValue("@id_list", ids);
+        }
+
+        public int Execute() => _command.ExecuteNonQuery();
+    }
+}
